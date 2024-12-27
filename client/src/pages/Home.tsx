b5b8b0b363f199201b3d02ff62423eb18841490a
Ruslana <ruslana.hural.j@gmail.com>
2024-12-27 02:23:28 +0100

    placeholder aboutMe section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Main from "../components/home/Main"
 import '../components/home/Home.scss';
 import Offers from "../components/home/Offers";
+import AboutMe from "../components/home/AboutMe";
 
 function Home() {
   return (
     <div className='home' lang="de">
       <Main />
       <Offers />
+      <AboutMe />
     </div>
   )
 }
