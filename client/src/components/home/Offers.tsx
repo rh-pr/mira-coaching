cc1bfc2a378f12f333c317bae31546746c37b33a
Ruslana <ruslana.hural.j@gmail.com>
2025-06-24 22:27:32 +0100

    change functionality offers-cards

--- client/src/components/home/Offers.tsx
+++ client/src/components/home/Offers.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import consultImg from '../../assets/img/prof/beratung.jpg';
 import workshopImg from '../../assets/img/prof/workshop.jpg';
 import teambuildImg from '../../assets/img/prof/team.jpeg';
@@ -7,11 +9,18 @@ import { profOffers } from '../../constants/main';
 const imags = [consultImg, workshopImg, teambuildImg];
 
 function Offers() {
+ const [openedIndex, setOpenedIndex] = useState<number | null>(null);
+
   return (
     <div className="offers">
       <h1>Professionelle Unterstützung für Ihre Teams</h1>
       <div className={`offers-cards`}>
-        {profOffers && profOffers.map((offer, ind) =>   <OffersCard  key={`off-${ind}`}  data={{img: imags[ind], name: offer.name, offers: offer.offers}}/>)}
+        {profOffers && profOffers.map((offer, ind) =>   
+          <OffersCard  key={`off-${ind}`}  
+                       data={{img: imags[ind], name: offer.name, offers: offer.offers}} 
+                       isOpen = {openedIndex === ind}
+                       showMore={() => setOpenedIndex(ind)}
+                       hideMore={() => setOpenedIndex(null)} />)}
       </div>
     </div>
   )
