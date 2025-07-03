cd3e49c0f9b5979ab2e4ef84d3412c538a9fba30
Ruslana <ruslana.hural.j@gmail.com>
2025-07-03 23:34:17 +0100

    created a basic config

---
+++ server/src/config/index.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+    port: process.env.PORT || 3000,
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+    dbUri: process.env.DB_URI || 'mongodb://localhost:27017/email_confirm_db',
+}
